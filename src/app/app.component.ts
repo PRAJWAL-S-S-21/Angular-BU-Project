199952683b5bc30bc378218ca41cfe8d7be1b9bf
Prajwal S S <prajwalsoodlu50@gmail.com>
2025-03-27 14:24:02 +0530

    added-navigation-page and icons in confirmation page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,4 @@ import { FormField } from './models/form-field.model';
 })
 export class AppComponent {
   title = 'member-registraion-form';
-
-  // fields: FormField[] = [
-  //   { name: 'firstName', label: 'First Name', type: 'text', show: true, required: true, order: 1 },
-  //   { name: 'lastName', label: 'Last Name', type: 'text', show: true, required: false, order: 2 },
-  //   { name: 'email', label: 'Email', type: 'email', show: true, required: true, order: 3 },
-  //   { name: 'phone', label: 'Phone', type: 'tel', show: false, required: false, order: 4 },
-  // ];
-
-  // updateFields(updatedFields: FormField[]) {
-  //   // Replace the fields array to trigger change detection
-  //   this.fields = [...updatedFields];
-  // }
-
 }
